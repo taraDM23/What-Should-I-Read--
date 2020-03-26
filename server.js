ac125cd28a12c3faa5002f5c95fc08127ece6920
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-26 16:22:04 +1100

    added mongouri

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://tam1:<Tara23>@ds235378.mlab.com:35378/heroku_rp9ltr83");
 
 // Start the API server
 app.listen(PORT, function() {
