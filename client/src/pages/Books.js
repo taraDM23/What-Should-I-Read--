5192ddc86597afe356007171291ef01a011947d7
tdm <55875354+taraDM23@users.noreply.github.com>
2020-03-26 15:52:23 +1100

    removed comments

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -8,16 +8,13 @@ import { List, ListItem } from "../components/List";
 import { Input, TextArea, FormBtn } from "../components/Form";
 
 function Books() {
-  // Setting our component's initial state
   const [books, setBooks] = useState([])
   const [formObject, setFormObject] = useState({})
 
-  // Load all books and store them with setBooks
   useEffect(() => {
     loadBooks()
   }, [])
 
-  // Loads all books and sets them to books
   function loadBooks() {
     API.getBooks()
       .then(res => 
@@ -26,21 +23,17 @@ function Books() {
       .catch(err => console.log(err));
   };
 
-  // Deletes a book from the database with a given id, then reloads books from the db
   function deleteBook(id) {
     API.deleteBook(id)
       .then(res => loadBooks())
       .catch(err => console.log(err));
   }
 
-  // Handles updating component state when the user types into the input field
   function handleInputChange(event) {
     const { name, value } = event.target;
     setFormObject({...formObject, [name]: value})
   };
 
-  // When the form is submitted, use the API.saveBook method to save the book data
-  // Then reload books from the database
   function handleFormSubmit(event) {
     event.preventDefault();
     if (formObject.title && formObject.author) {
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -7,8 +7,6 @@ import API from "../utils/API";
 function Detail(props) {
   const [book, setBook] = useState({})
 
-  // When this component mounts, grab the book with the _id of props.match.params.id
-  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
   useEffect(() => {
     API.getBook(props.match.params.id)
       .then(res => setBook(res.data ))
